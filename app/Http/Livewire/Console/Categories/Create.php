947b0829e6e394de2b474603992685583b2c2be2
teddinata <teddinata@users.noreply.github.com>
2023-12-16 05:47:49 +0700

    fix: upload img

--- app/Http/Livewire/Console/Categories/Create.php
+++ app/Http/Livewire/Console/Categories/Create.php
@@ -26,19 +26,54 @@ class Create extends Component
         // if (!in_array($extension, ['png', 'jpeg', 'bmp', 'gif', 'jpg'])) {
         //     $this->reset('image');
         // }
-        $this->validate([
-            'image' => 'image|max:102400', // .5MB Max
-            // 'image'    => 'required|image',
-            'name'     => 'required',
-        ]);
+        // $this->validate([
+        //     'image' => 'image|max:102400', // .5MB Max
+        //     // 'image'    => 'required|image',
+        //     'name'     => 'required',
+        // ]);
 
+        // $this->image->store('public/categories');
+
+        // $category = Category::create([
+        //     'name' => $this->name,
+        //     'slug' => Str::slug($this->name, '-'),
+        //     'image'=> $this->image->hashName()
+        // ]);
+        // Validasi apakah file gambar diunggah
+    $this->validate([
+        'image' => 'required|image|max:102400', // .5MB Max
+        'name'  => 'required',
+    ]);
+
+    // Pengecekan apakah file gambar tersedia
+    if ($this->image) {
+        $extension = pathinfo($this->image->getFilename(), PATHINFO_EXTENSION);
+
+        // Validasi ekstensi file gambar
+        if (!in_array($extension, ['png', 'jpeg', 'bmp', 'gif', 'jpg'])) {
+            $this->reset('image');
+            // Atau tambahkan pesan kesalahan jika perlu
+            $this->addError('image', 'Invalid image format. Please upload a valid image.');
+            return;
+        }
+
+        // Lanjutkan dengan penyimpanan dan pembuatan Category jika validasi berhasil
         $this->image->store('public/categories');
 
         $category = Category::create([
-            'name' => $this->name,
-            'slug' => Str::slug($this->name, '-'),
-            'image'=> $this->image->hashName()
+            'name'  => $this->name,
+            'slug'  => Str::slug($this->name, '-'),
+            'image' => $this->image->hashName(),
         ]);
+
+        // Tambahkan logika atau tindakan lain setelah pembuatan Category
+    }
+
+    // Jika file gambar tidak diunggah, tangani sesuai kebutuhan
+    // ...
+
+    // Reset nilai input setelah proses selesai
+    $this->reset(['name', 'image']);
 
         if($category) {
             session()->flash('success', 'Data saved successfully');
